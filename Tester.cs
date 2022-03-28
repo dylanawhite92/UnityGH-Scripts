ae053e718a3bd285a95900d291285aa0f0d84cc1
The Crystal King <dylanawhite11@gmail.com>
2022-03-28 14:58:28 -0500

    variables added

---
+++ Tester.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// class
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Variables - seem to be literally typed instead of like let and const
+
+    // float point num, 32-bit
+    float power = 2.15464644f;
+    // double, more precise 64-bit decimal that allows for more places in decimal.
+    double health = 3.556463135469794616216;
+    // integer
+    int score = 10;
+
+    // string
+    string playerName = "John Darksouls";
+
+    // boolean
+    bool iAlreadyKnowAllOfThis = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
